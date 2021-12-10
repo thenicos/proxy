b8ccbacc40afa52f23e2f1d32f4422ac76b3a8c8
Niccolò Musso Barcucci <niccolo.mussobarcucci@gmail.com>
2021-12-10 20:09:51 +0100

    cors enabled

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 import os
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 #@basic_auth.required
